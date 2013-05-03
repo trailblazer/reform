c80eff9f1b6cf2d13396e4a697ab8552088daf6b
Nick Sutterer <apotonick@gmail.com>
2013-05-03 16:23:33 +1000

    merge property and properties test.

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -4,38 +4,15 @@ class DslTest < MiniTest::Spec
   class SongForm < Reform::Form
     include DSL
 
-    property :title,  :on => :song
-    property :name,   :on => :artist
+    property  :title,  :on => :song
+    properties [:name, :genre],   :on => :artist
 
-    validates :name, :title, presence: true
+    validates :name, :title, :genre, presence: true
   end
 
   let (:form) { SongForm.new(:song => OpenStruct.new(:title => "Rio"), :artist => OpenStruct.new()) }
 
-  it "what" do
-    form.validate("title" => "Greyhound").must_equal false
-  end
-
-  describe "::properties" do
-    class SuperSongForm < Reform::Form
-      include DSL
-
-      properties [:title, :year], on: :song
-      property :name, on: :artist
-
-      validates :year, :title, presence: true
-    end
-
-    let (:form) {
-      SuperSongForm.new(
-        :song => OpenStruct.new(:title => "Rio", :year => 1990),
-        :artist => OpenStruct.new()
-      )
-    }
-
-    it "works with an array of properties" do
-      form.validate("title" => "Greyhound", "year" => 1990).must_equal true
-    end
-
+  it "works by creating Representer and Composition for you" do
+    form.validate("title" => "Greyhound", "name" => "Frenzal Rhomb").must_equal false
   end
 end
