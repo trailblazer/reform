77504e5c967b027646fbeaab2f02d55ebf8b3771
Nick Sutterer <apotonick@gmail.com>
2013-05-06 16:00:58 +1000

    fix a bug where ActiveModel::model wouldn't do what we want it to do.

--- lib/reform/form/active_model.rb
+++ lib/reform/form/active_model.rb
@@ -10,6 +10,7 @@ module Reform::Form::ActiveModel
       @model_options = args  # FIXME: make inheritable!
 
       delegate "persisted?", :to_key, :to_param, :to => args.last[:on]
+      alias_method args.first, args.last[:on] # delegate #hit to #song to #model.
     end
 
     def property(name, options={})
--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -17,38 +17,43 @@ class DslTest < MiniTest::Spec
   end
 end
 
-require 'reform/form/active_model'
 class ActiveModelTest < MiniTest::Spec
-   class SongForm < Reform::Form
+   class HitForm < Reform::Form
     include DSL
     include Reform::Form::ActiveModel
 
     property  :title,  :on => :song
     properties [:name, :genre],   :on => :artist
 
-    model :song, :on => :song
+    model :hit, :on => :song
   end
 
   let (:rio) { OpenStruct.new(:title => "Rio") }
-  let (:form) { SongForm.new(:song => rio, :artist => OpenStruct.new) }
+  let (:duran) { OpenStruct.new }
+  let (:form) { HitForm.new(:song => rio, :artist => duran) }
 
   it "creates model readers" do
+    form.hit.must_equal rio
+  end
+
+  it "creates composition readers" do
     form.song.must_equal rio
+    form.artist.must_equal duran
   end
 
   it "provides ::model_name" do
-    form.class.model_name.must_equal "Song"
+    form.class.model_name.must_equal "Hit"
   end
 
   it "provides #persisted?" do
-    SongForm.new(:song => OpenStruct.new.instance_eval { def persisted?; "yo!"; end; self }, :artist => OpenStruct.new).persisted?.must_equal "yo!"
+    HitForm.new(:song => OpenStruct.new.instance_eval { def persisted?; "yo!"; end; self }, :artist => OpenStruct.new).persisted?.must_equal "yo!"
   end
 
   it "provides #to_key" do
-    SongForm.new(:song => OpenStruct.new.instance_eval { def to_key; "yo!"; end; self }, :artist => OpenStruct.new).to_key.must_equal "yo!"
+    HitForm.new(:song => OpenStruct.new.instance_eval { def to_key; "yo!"; end; self }, :artist => OpenStruct.new).to_key.must_equal "yo!"
   end
 
   it "provides #to_param" do
-    SongForm.new(:song => OpenStruct.new.instance_eval { def to_param; "yo!"; end; self }, :artist => OpenStruct.new).to_param.must_equal "yo!"
+    HitForm.new(:song => OpenStruct.new.instance_eval { def to_param; "yo!"; end; self }, :artist => OpenStruct.new).to_param.must_equal "yo!"
   end
 end
