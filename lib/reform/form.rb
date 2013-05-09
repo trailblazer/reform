5ec2318d20a2a40c4666f1a59265037f9311cb54
Timo Schilling <timo@schilling.io>
2013-05-09 02:39:02 +0200

    move Reform::Composition code into a seperate file

--- lib/reform/form.rb
+++ lib/reform/form.rb
@@ -1,5 +1,6 @@
 require 'delegate'
 require 'reform/fields'
+require 'reform/composition'
 
 module Reform
   class Form  < SimpleDelegator
@@ -50,60 +51,6 @@ module Reform
     # FIXME: make AM optional.
     require 'active_model'
     include ActiveModel::Validations
-  end
-
-  # Keeps composition of models and knows how to transform a plain hash into a nested hash.
-  class Composition
-    class << self
-      def map(options)
-        @attr2obj = {}  # {song: ["title", "track"], artist: ["name"]}
-
-        options.each do |mdl, meths|
-          create_accessors(mdl, meths)
-          attr_reader mdl # FIXME: unless already defined!!
-
-          meths.each { |m| @attr2obj[m.to_s] = mdl }
-        end
-      end
-
-      # Specific to representable.
-      def map_from(representer)
-        options = {}
-        representer.representable_attrs.each do |cfg|
-          options[cfg.options[:on]] ||= []
-          options[cfg.options[:on]] << cfg.name
-        end
-
-        map options
-      end
-
-      def model_for_property(name)
-        @attr2obj.fetch(name.to_s)
-      end
-
-    private
-      def create_accessors(model, methods)
-        accessors = methods.collect { |m| [m, "#{m}="] }.flatten
-        delegate *accessors, to: "@#{model}"
-      end
-    end
-
-    # TODO: make class method?
-    def nested_hash_for(attrs)
-      {}.tap do |hsh|
-        attrs.each do |name, val|
-          obj = self.class.model_for_property(name)
-          hsh[obj] ||= {}
-          hsh[obj][name.to_sym] = val
-        end
-      end
-    end
-
-    def initialize(models)
-      models.each do |name, obj|
-        instance_variable_set(:"@#{name}", obj)
-      end
-    end
   end
 
   require 'representable/hash'
