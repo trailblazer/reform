1190308f715e9abcfbe3aeb10dab18439065b98b
Philip Arndt <parndt@gmail.com>
2013-05-13 04:45:34 +1200

    Remove unneeded module declaration; require reform/form before specifying it.

--- lib/reform.rb
+++ lib/reform.rb
@@ -1,9 +1,4 @@
-require "reform/version"
-
-module Reform
-  # Your code goes here...
-end
-
+require 'reform/version'
 require 'reform/form'
 require 'reform/form/dsl'
 require 'reform/form/active_model'
