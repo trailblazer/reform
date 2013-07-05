a5096b7cde0dcd6e539fcc1160231fd44ba8e56e
Nick Sutterer <apotonick@gmail.com>
2013-07-05 16:25:32 +1000

    simplify coercion test.

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -52,11 +52,10 @@ class DslTest < MiniTest::Spec
   require 'reform/form/coercion'
   it "allows coercion" do
     form = Class.new(Reform::Form) do
-      include Reform::Form::DSL
       include Reform::Form::Coercion
 
-      property :written_at, :type => DateTime, :on => :song
-    end.new(:song => OpenStruct.new(:written_at => "31/03/1981"))
+      property :written_at, :type => DateTime
+    end.new(OpenStruct.new(:written_at => "31/03/1981"))
 
     form.written_at.must_be_kind_of DateTime
     form.written_at.must_equal DateTime.parse("Tue, 31 Mar 1981 00:00:00 +0000")
@@ -64,11 +63,10 @@ class DslTest < MiniTest::Spec
 
   it "allows coercion in validate" do
     form = Class.new(Reform::Form) do
-      include Reform::Form::DSL
       include Reform::Form::Coercion
 
-      property :id, :type => Integer, :on => :song
-    end.new(:song => OpenStruct.new())
+      property :id, :type => Integer
+    end.new(OpenStruct.new())
 
     form.validate("id" => "1")
     form.to_hash.must_equal("id" => 1)
