b30d3203085ae7338bcaa99ed9f0fc4ee58442cc
Nick Sutterer <apotonick@gmail.com>
2013-08-27 16:19:01 +1000

    make rails test work.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 require 'reform'
 require 'minitest/autorun'
 
-require "dummy/config/environment"
-
 class ReformSpec < MiniTest::Spec
   let (:duran)  { OpenStruct.new(:name => "Duran Duran") }
   let (:rio)    { OpenStruct.new(:title => "Rio") }
