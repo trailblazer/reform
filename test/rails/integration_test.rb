743a8041d79326a1fd1495ba8951d09a9321df0d
Nick Sutterer <apotonick@gmail.com>
2013-09-12 14:54:26 +1000

    integration test is ready for @gogogarrett.

--- test/rails/integration_test.rb
+++ test/rails/integration_test.rb
@@ -3,17 +3,41 @@ require_relative "../test_helper"
 require "dummy/config/environment"
 require "rails/test_help" # adds stuff like @routes, etc.
 
- #  ActiveRecord::Schema.define do
-      #    create_table :artists do |table|
-      #      table.column :name, :string
-      #      table.timestamps
-      #    end
-      #  end
+require "haml"
+require "haml/template" # Thanks, Nathan!
+
+#ActiveRecord::Schema.define do
+  # create_table :artists do |table|
+  #   table.column :name, :string
+  #   table.timestamps
+  # end
+
+#   create_table :songs do |table|
+#     table.column :title, :string
+#     table.column :album_id, :integer
+#   end
+
+#   create_table :albums do |table|
+#     table.column :title, :string
+#   end
+# end
 
 class RailsTest < ActionController::TestCase
   tests MusicianController
 
-  test "bla" do
-    get :index
+  # test "bla" do
+  #   get :index
+  # end
+
+  # test "rendering 1-n" do
+  #   get :album_new
+  # end
+end
+
+class HasOneAndHasManyTest < ActionController::TestCase
+  tests AlbumsController
+
+  test "rendering 1-1 and 1-n" do
+    get :new
   end
 end
