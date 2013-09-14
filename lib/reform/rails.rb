4fdafd0cccb8600a24317d9442403f9a4bfb5a58
Nick Sutterer <apotonick@gmail.com>
2013-09-14 12:49:33 +1000

    include AM in rails env.

--- lib/reform/rails.rb
+++ lib/reform/rails.rb
@@ -4,5 +4,6 @@ if defined?(ActiveRecord)
 end
 
 Reform::Form.class_eval do # DISCUSS: i'd prefer having a separate Rails module to be mixed into the Form but this is way more convenient for 99% users.
+  include Reform::Form::ActiveModel
   include Reform::Form::ActiveModel::FormBuilderMethods
 end
