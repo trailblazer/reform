4d2e7001f282ecf2ee68becc89029c44cda6ff24
Nick Sutterer <apotonick@gmail.com>
2014-04-17 15:19:56 +1000

    use Disposable::Composition in Reform::Composition.

--- lib/reform/composition.rb
+++ lib/reform/composition.rb
@@ -1,18 +1,10 @@
 module Reform
   # Keeps composition of models and knows how to transform a plain hash into a nested hash.
   class Composition
+    require 'disposable/composition' # FIXME: what about autoloading?
+    include Disposable::Composition
+
     class << self
-      def map(options)
-        @attr2obj = {}  # {song: ["title", "track"], artist: ["name"]}
-
-        options.each do |mdl, meths|
-          create_accessors(mdl, meths)
-          attr_reader mdl
-
-          meths.each { |m| @attr2obj[m.to_s] = mdl }
-        end
-      end
-
       # Specific to representable.
       def map_from(representer)
         options = {}
@@ -27,12 +19,6 @@ module Reform
       def model_for_property(name)
         @attr2obj.fetch(name.to_s)
       end
-
-    private
-      def create_accessors(model, methods)
-        accessors = methods.collect { |m| [m, "#{m}="] }.flatten
-        delegate *accessors << {:to => :"#{model}"}
-      end
     end
 
     # TODO: make class method?
@@ -43,12 +29,6 @@ module Reform
           hsh[obj] ||= {}
           hsh[obj][name.to_sym] = val
         end
-      end
-    end
-
-    def initialize(models)
-      models.each do |name, obj|
-        instance_variable_set(:"@#{name}", obj)
       end
     end
   end
