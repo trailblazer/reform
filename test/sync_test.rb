5b6101f290bf1fb04d82fee5fc3441c94eb003c4
Nick Sutterer <apotonick@gmail.com>
2014-04-25 10:43:51 +1000

    add #sync test with deep-nesting.

--- test/sync_test.rb
+++ test/sync_test.rb
@@ -6,14 +6,17 @@ class SyncTest < BaseTest
       {
         "title" => "Best Of",
         "hit"   => {"title" => "Roxanne"},
-        "songs" => [{"title" => "Fallout"}, {"title" => "Roxanne"}]
+        "songs" => [{"title" => "Fallout"}, {"title" => "Roxanne"}],
+        :band  => {:label => {:name => "Polydor"}}
       }
     }
 
-    let (:album) { Album.new(nil, hit, [song1, song2]) }
+    let (:album) { Album.new(nil, hit, [song1, song2], band) }
     let (:hit) { Song.new }
     let (:song1) { Song.new }
     let (:song2) { Song.new }
+    let (:band) { Band.new(label) }
+    let (:label) { Label.new }
 
     subject { ErrorsTest::AlbumForm.new(album) }
 
@@ -26,5 +29,6 @@ class SyncTest < BaseTest
     it { hit.title.must_equal "Roxanne" }
     it { song1.title.must_equal "Fallout" }
     it { song2.title.must_equal "Roxanne" }
+    it { label.name.must_equal "Polydor" }
   end
 end
