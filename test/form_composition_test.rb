eebc99748f2d12ee41c8346d8d7a68a6ea912f44
Nick Sutterer <apotonick@gmail.com>
2014-04-30 13:01:00 +1000

    add a test with :empty with Composition to make sure it works. fixes #75.

--- test/form_composition_test.rb
+++ test/form_composition_test.rb
@@ -11,8 +11,9 @@ class FormCompositionTest < MiniTest::Spec
     property  :requester_id,  :on => :requester, :as => :id
     properties [:title, :id], :on => :song
     # property  :channel # FIXME: what about the "main model"?
+    property :channel, :empty => true, :on => :song
 
-    validates :name, :title, :presence => true
+    validates :name, :title, :channel, :presence => true
   end
 
   let (:form)   { RequestForm.new(:song => song, :requester => requester) }
@@ -25,6 +26,7 @@ class FormCompositionTest < MiniTest::Spec
   it { form.title.must_equal "Rio" }
   it { form.name.must_equal "Duran Duran" }
   it { form.requester_id.must_equal 2 }
+  it { form.channel.must_equal nil }
 
   # delegation form -> composed models (e.g. when saving this can be handy)
   it { form.song.must_equal   song }
@@ -48,13 +50,15 @@ class FormCompositionTest < MiniTest::Spec
     end
 
     it "provides nested symbolized hash as second block argument" do
+      form.validate("title" => "Greyhound", "name" => "Frenzal Rhomb", "channel" => "JJJ")
+
       hash = {}
 
       form.save do |data, map|
         hash = map
       end
 
-      hash.must_equal({:song=>{:title=>"Rio", :id=>1}, :requester=>{:name=>"Duran Duran", :id=>2}})
+      hash.must_equal({:song=>{:title=>"Greyhound", :id=>1, :channel => "JJJ"}, :requester=>{:name=>"Frenzal Rhomb", :id=>2}})
     end
 
     it "pushes data to models when no block passed" do
