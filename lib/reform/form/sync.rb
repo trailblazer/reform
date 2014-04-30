1cc948bec1aaf2fba0f6fc77a797ace06f0a9ec5
Nick Sutterer <apotonick@gmail.com>
2014-04-30 14:47:52 +1000

    correct indentation.

--- lib/reform/form/sync.rb
+++ lib/reform/form/sync.rb
@@ -4,56 +4,56 @@ class Reform::Form
   #   2. call sync! on nested
   module Sync
     # Mechanics for writing input to model.
-      # Writes input to model.
-      module Writer
-        def from_hash(*)
-          # process output from InputRepresenter {title: "Mint Car", hit: <Form>}
-          # and just call sync! on nested forms.
-          nested_forms do |attr|
-            attr.merge!(
-              :instance     => lambda { |fragment, *| fragment },
-              :deserialize => lambda { |object, *| object.sync! },
-            )
-          end
-
-          super
+    # Writes input to model.
+    module Writer
+      def from_hash(*)
+        # process output from InputRepresenter {title: "Mint Car", hit: <Form>}
+        # and just call sync! on nested forms.
+        nested_forms do |attr|
+          attr.merge!(
+            :instance     => lambda { |fragment, *| fragment },
+            :deserialize => lambda { |object, *| object.sync! },
+          )
         end
-      end
 
-      # Transforms form input into what actually gets written to model.
-      # output: {title: "Mint Car", hit: <Form>}
-      module InputRepresenter
-        include Reform::Representer::WithOptions
-        # TODO: make dynamic.
-        include EmptyAttributesOptions
-        include ReadonlyAttributesOptions
-
-        def to_hash(*)
-          nested_forms do |attr|
-            attr.merge!(
-              :representable  => false,
-              :prepare        => lambda { |obj, *| obj }
-            )
-
-          end
-
-          super
-        end
+        super
       end
     end
 
-    ### TODO: add ToHash with :prepare => lambda { |form, args| form },
+    # Transforms form input into what actually gets written to model.
+    # output: {title: "Mint Car", hit: <Form>}
+    module InputRepresenter
+      include Reform::Representer::WithOptions
+      # TODO: make dynamic.
+      include EmptyAttributesOptions
+      include ReadonlyAttributesOptions
 
-    def sync_models
-      sync!
+      def to_hash(*)
+        nested_forms do |attr|
+          attr.merge!(
+            :representable  => false,
+            :prepare        => lambda { |obj, *| obj }
+          )
+
+        end
+
+        super
+      end
     end
-    alias_method :sync, :sync_models
+  end
 
-    def sync! # semi-public.
-      input_representer = mapper.new(self).extend(InputRepresenter)
+  ### TODO: add ToHash with :prepare => lambda { |form, args| form },
 
-      input = input_representer.to_hash
+  def sync_models
+    sync!
+  end
+  alias_method :sync, :sync_models
 
-      mapper.new(model).extend(Writer).from_hash(input)
-    end
+  def sync! # semi-public.
+    input_representer = mapper.new(self).extend(InputRepresenter)
+
+    input = input_representer.to_hash
+
+    mapper.new(model).extend(Writer).from_hash(input)
+  end
 end
