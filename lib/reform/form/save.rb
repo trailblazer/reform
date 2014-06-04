bf236ffc9fd5725126489e5aa5709e0f7f6d0985
Nick Sutterer <apotonick@gmail.com>
2014-06-04 12:57:05 +1000

    use a fully-qualified module name for Save and Sync. move #sync_models into Sync, it was defined in Form, to make it usable with Contracts.

--- lib/reform/form/save.rb
+++ lib/reform/form/save.rb
@@ -1,61 +1,59 @@
-Reform::Form.class_eval do
-  module Save
-    module RecursiveSave
-      def to_hash(*)
-        # process output from InputRepresenter {title: "Mint Car", hit: <Form>}
-        # and just call sync! on nested forms.
-        nested_forms do |attr|
-          attr.merge!(
-            :instance  => lambda { |fragment, *| fragment },
-            :serialize => lambda { |object, args| object.save! unless args.binding[:save] === false },
-          )
-        end
-
-        super
+module Reform::Form::Save
+  module RecursiveSave
+    def to_hash(*)
+      # process output from InputRepresenter {title: "Mint Car", hit: <Form>}
+      # and just call sync! on nested forms.
+      nested_forms do |attr|
+        attr.merge!(
+          :instance  => lambda { |fragment, *| fragment },
+          :serialize => lambda { |object, args| object.save! unless args.binding[:save] === false },
+        )
       end
+
+      super
     end
+  end
 
-    def save
-      # DISCUSS: we should never hit @mapper here (which writes to the models) when a block is passed.
-      return yield self, to_nested_hash if block_given?
+  def save
+    # DISCUSS: we should never hit @mapper here (which writes to the models) when a block is passed.
+    return yield self, to_nested_hash if block_given?
 
-      sync_models # recursion
-      save!
-    end
+    sync_models # recursion
+    save!
+  end
 
-    def save!
-      save_model
-      mapper.new(self).extend(RecursiveSave).to_hash # save! on all nested forms.
-    end
+  def save!
+    save_model
+    mapper.new(self).extend(RecursiveSave).to_hash # save! on all nested forms.
+  end
 
-    def save_model
-      model.save # TODO: implement nested (that should really be done by Twin/AR).
-    end
+  def save_model
+    model.save # TODO: implement nested (that should really be done by Twin/AR).
+  end
 
 
-    module NestedHash
-      def to_hash(*)
-        # Transform form data into a nested hash for #save.
-        nested_forms do |attr|
-          attr.merge!(
-            :instance  => lambda { |fragment, *| fragment },
-            :serialize => lambda { |object, args| object.to_nested_hash },
-          )
-        end
-
-        representable_attrs.each do |attr|
-          attr.merge!(:as => attr[:private_name] || attr.name)
-        end
-
-        super
+  module NestedHash
+    def to_hash(*)
+      # Transform form data into a nested hash for #save.
+      nested_forms do |attr|
+        attr.merge!(
+          :instance  => lambda { |fragment, *| fragment },
+          :serialize => lambda { |object, args| object.to_nested_hash },
+        )
       end
-    end
 
-    require "active_support/hash_with_indifferent_access" # DISCUSS: replace?
-    def to_nested_hash
-      map = mapper.new(self).extend(Save::NestedHash)
+      representable_attrs.each do |attr|
+        attr.merge!(:as => attr[:private_name] || attr.name)
+      end
 
-      ActiveSupport::HashWithIndifferentAccess.new(map.to_hash)
+      super
     end
+  end
+
+  require "active_support/hash_with_indifferent_access" # DISCUSS: replace?
+  def to_nested_hash
+    map = mapper.new(self).extend(NestedHash)
+
+    ActiveSupport::HashWithIndifferentAccess.new(map.to_hash)
   end
 end
--- lib/reform/form/sync.rb
+++ lib/reform/form/sync.rb
@@ -1,49 +1,48 @@
-Reform::Form.class_eval do
-  # #sync!
-  #   1. assign scalars to model (respecting virtual, excluded attributes)
-  #   2. call sync! on nested
-  module Sync
-    # Mechanics for writing input to model.
-    # Writes input to model.
-    module Writer
-      def from_hash(*)
-        # process output from InputRepresenter {title: "Mint Car", hit: <Form>}
-        # and just call sync! on nested forms.
-        nested_forms do |attr|
-          attr.merge!(
-            :instance     => lambda { |fragment, *| fragment },
-            :deserialize => lambda { |object, *| object.sync! },
-            :setter => lambda { |*| } # don't write hit=<Form>.
-          )
-        end
-
-        super
+# #sync!
+#   1. assign scalars to model (respecting virtual, excluded attributes)
+#   2. call sync! on nested
+module Reform::Form::Sync
+  # Mechanics for writing input to model.
+  # Writes input to model.
+  module Writer
+    def from_hash(*)
+      # process output from InputRepresenter {title: "Mint Car", hit: <Form>}
+      # and just call sync! on nested forms.
+      nested_forms do |attr|
+        attr.merge!(
+          :instance     => lambda { |fragment, *| fragment },
+          :deserialize => lambda { |object, *| object.sync! },
+          :setter => lambda { |*| } # don't write hit=<Form>.
+        )
       end
-    end
 
-    # Transforms form input into what actually gets written to model.
-    # output: {title: "Mint Car", hit: <Form>}
-    module InputRepresenter
-      include Reform::Representer::WithOptions
-      # TODO: make dynamic.
-      include Reform::Form::EmptyAttributesOptions
-      include Reform::Form::ReadonlyAttributesOptions
-
-      def to_hash(*)
-        nested_forms do |attr|
-          attr.merge!(
-            :representable  => false,
-            :prepare        => lambda { |obj, *| obj }
-          )
-
-        end
-
-        super
-      end
+      super
     end
   end
 
-  ### TODO: add ToHash with :prepare => lambda { |form, args| form },
+  # Transforms form input into what actually gets written to model.
+  # output: {title: "Mint Car", hit: <Form>}
+  module InputRepresenter
+    include Reform::Representer::WithOptions
+    # TODO: make dynamic.
+    include Reform::Form::EmptyAttributesOptions
+    include Reform::Form::ReadonlyAttributesOptions
+
+    def to_hash(*)
+      nested_forms do |attr|
+        attr.merge!(
+          :representable  => false,
+          :prepare        => lambda { |obj, *| obj }
+        )
+
+      end
+
+      super
+    end
+  end
+
+
+### TODO: add ToHash with :prepare => lambda { |form, args| form },
 
   def sync_models
     sync!
@@ -51,10 +50,10 @@ Reform::Form.class_eval do
   alias_method :sync, :sync_models
 
   def sync! # semi-public.
-    input_representer = mapper.new(self).extend(Sync::InputRepresenter)
+    input_representer = mapper.new(self).extend(InputRepresenter)
 
     input = input_representer.to_hash
 
-    mapper.new(aliased_model).extend(Sync::Writer).from_hash(input)
+    mapper.new(aliased_model).extend(Writer).from_hash(input)
   end
 end
