e4f34a1f68273f7e9fad3495a416f58431c99294
Nick Sutterer <apotonick@gmail.com>
2014-06-07 14:10:56 +1000

    make accessor deprecation messages more meaningful.

--- lib/reform/form/sync.rb
+++ lib/reform/form/sync.rb
@@ -63,31 +63,35 @@ module Reform::Form::Sync
 
   def deprecate_potential_readers_used_in_sync_or_save(fields) # TODO: remove in 1.1.
     readers = []
+
     mapper.representable_attrs.each do |definition|
       return fields if definition[:presentation_accessors]
 
       name = definition.name
-      return fields if method(name).source_location.inspect =~ /forwardable/ # defined by Reform, not overridden by user.
+      next if method(name).source_location.inspect =~ /forwardable/ # defined by Reform, not overridden by user.
 
       readers << name
     end
+    return fields if readers.size == 0
 
-    warn "[Reform] Deprecation: You're overriding the following readers: #{readers.join(', ')}. In Reform 1.1, those readers will be used for presentation in the view, only. In case you are using the readers deliberately to modify incoming data for #save or #sync: this won't work anymore. If you know what you're doing, add `presentation_accessors: true` to a property to suppress this message and use the new behaviour."
+    warn "[Reform] Deprecation: You're overriding the following readers: #{readers.join(', ')}. In Reform 1.1, those readers will be used for presentation in the view, only. In case you are using the readers deliberately to modify incoming data for #save or #sync: this won't work anymore. If you just use the custom readers in the form view, add `presentation_accessors: true` to a property to suppress this message and use the new behaviour."
 
     self # old mode
   end
   def deprecate_potential_writers_used_in_validate(fields) # TODO: remove in 1.1.
     readers = []
+
     mapper.representable_attrs.each do |definition|
       return fields if definition[:presentation_accessors]
 
       name = definition.setter
-      return fields if method(name).source_location.inspect =~ /forwardable/ # defined by Reform, not overridden by user.
+      next if method(name).source_location.inspect =~ /forwardable/ # defined by Reform, not overridden by user.
 
       readers << name
     end
+    return fields if readers.size == 0
 
-    warn "[Reform] Deprecation: You're overriding the following writers: #{readers.join(', ')}. In Reform 1.1, those writers will be used for presentation in the view, only. In case you are using the writers deliberately to modify incoming data for #vaidate: this won't work anymore. If you know what you're doing, add `presentation_accessors: true` to a property to suppress this message and use the new behaviour."
+    warn "[Reform] Deprecation: You're overriding the following writers: #{readers.join(', ')}. In Reform 1.1, those writers will be used for presentation in the view, only. In case you are using the writers deliberately to modify incoming data for #setup or #validate: this won't work anymore.  Add `presentation_accessors: true` to a property to suppress this message and use the new behaviour."
 
     self # old mode
   end
