172c38dfd78f95424e8f0f3a07b4bf8e2c8c8995
Nick Sutterer <apotonick@gmail.com>
2014-07-26 13:30:01 +1000

    remove puts.

--- lib/reform/form/active_record.rb
+++ lib/reform/form/active_record.rb
@@ -37,8 +37,6 @@ module Reform::Form::ActiveRecord
   def model_for_property(name)
     return model unless is_a?(Reform::Form::Composition) # i am too lazy for proper inheritance. there should be a ActiveRecord::Composition that handles this.
 
-    puts mapper.representable_attrs.inspect
-    puts "***************************"
     model_name = mapper.representable_attrs.get(name)[:on]
     model[model_name]
   end
