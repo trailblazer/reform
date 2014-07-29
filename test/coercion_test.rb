7481f0c43775e80de4d2d03fff226e3abbff88d6
Nick Sutterer <apotonick@gmail.com>
2014-07-29 15:14:31 +1000

    better coercion test.

--- test/coercion_test.rb
+++ test/coercion_test.rb
@@ -2,43 +2,65 @@ require "test_helper"
 require "reform/form/coercion"
 
 class CoercionTest < BaseTest
+  class Form < Reform::Form
+    include Coercion
+
+    property :released_at, :type => DateTime
+
+    property :hit do
+      # include Coercion
+      property :length, :type => Integer
+      property :good,   :type => Virtus::Attribute::Boolean
+    end
+
+    property :band do
+      include Coercion
+      property :label do
+        include Coercion
+        property :value, :type => Float
+      end
+    end
+  end
+
   subject do
-    Class.new(Reform::Form) do
-      include Reform::Form::Coercion
+    Form.new(album)
+  end
 
-      property :released_at, :type => DateTime
-
-      property :hit do
-        property :length, :type => Integer
-        property :good,   :type => Virtus::Attribute::Boolean
-      end
-
-      property :band do
-        property :label do
-          property :value, :type => Float
-        end
-      end
-    end.new(OpenStruct.new(
+  let (:album) {
+    OpenStruct.new(
       :released_at => "31/03/1981",
-      :hit => OpenStruct.new(:length => "312"),
-      :band => Band.new(OpenStruct.new(:value => "9999.99"))
-    ))
+      :hit         => OpenStruct.new(:length => "312"),
+      :band        => Band.new(OpenStruct.new(:value => "9999.99"))
+    )
+  }
+
+  # it { subject.released_at.must_be_kind_of DateTime }
+  it { subject.released_at.must_equal "31/03/1981" } # NO coercion in setup.
+  it { subject.hit.length.must_equal "312" }
+  it { subject.band.label.value.must_equal "9999.99" }
+
+
+  let (:params) {
+    {
+      :released_at => "30/03/1981",
+      :hit         => {:length => "312"},
+      :band        => {:label => {:value => "9999.99"}}
+    }
+  }
+
+
+  # validate
+  describe "#validate" do
+    before { subject.validate(params) }
+
+    it { subject.released_at.must_equal DateTime.parse("30/03/1981") }
+    it { subject.hit.length.must_equal 312 }
+    it { subject.hit.good.must_equal nil }
+    it { subject.band.label.value.must_equal 9999.99 }
   end
 
-  it { subject.released_at.must_be_kind_of DateTime }
-  it { subject.released_at.must_equal DateTime.parse("Tue, 31 Mar 1981 00:00:00 +0000") }
-  it { subject.hit.length.must_equal 312 }
-  it { subject.band.label.value.must_equal 9999.99 }
+  # save
 
 
-  it "allows coercion in validate" do
-    form = Class.new(Reform::Form) do
-      include Reform::Form::Coercion
 
-      property :id, :type => Integer
-    end.new(OpenStruct.new())
-
-    form.validate("id" => "1")
-    form.id.must_equal 1
-  end
 end
