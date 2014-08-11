d609b626ec8b8f66c4203e707cda4ddf2b8b270c
Emil Sågfors <emil.sagfors@iki.fi>
2014-08-11 15:33:22 +0300

    Automatically include ActiveRecord module in reform/rails

--- lib/reform/rails.rb
+++ lib/reform/rails.rb
@@ -6,4 +6,5 @@ end
 Reform::Form.class_eval do # DISCUSS: i'd prefer having a separate Rails module to be mixed into the Form but this is way more convenient for 99% users.
   include Reform::Form::ActiveModel
   include Reform::Form::ActiveModel::FormBuilderMethods
+  include Reform::Form::ActiveRecord if defined?(ActiveRecord)
 end
