f1c5654bea510cca7d498fc9041a528144fa5327
Nick Sutterer <apotonick@gmail.com>
2014-08-23 10:36:07 +1000

    better readable.

--- lib/reform/contract.rb
+++ lib/reform/contract.rb
@@ -114,8 +114,10 @@ module Reform
       # representer_class.inherit_module!(representer)
       representer.representable_attrs.each do |dfn|
         next if dfn.name == "links" # wait a second # FIXME what is that?
+
         # TODO: remove manifesting and do that in representable, too!
         args = [dfn.name, dfn.instance_variable_get(:@options)] # TODO: dfn.to_args (inluding &block)
+
         property(*args) and next unless dfn.representer_module
         property(*args) { include dfn.representer_module } # nested.
       end
