f9aff6c86746f48f66f0ebca8b8062c98a6a29d2
Nick Sutterer <apotonick@gmail.com>
2014-10-01 13:20:39 +1000

    require model_validations in reform/active_record.

--- lib/reform/rails.rb
+++ lib/reform/rails.rb
@@ -1,6 +1,6 @@
 require 'reform/form/active_model'
 
-require 'reform/form/active_record' if defined?(ActiveRecord)
+require 'reform/active_record' if defined?(ActiveRecord)
 
 Reform::Form.class_eval do # DISCUSS: i'd prefer having a separate Rails module to be mixed into the Form but this is way more convenient for 99% users.
   include Reform::Form::ActiveModel
