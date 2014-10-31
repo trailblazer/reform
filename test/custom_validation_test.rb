1a0f82df8138c0fe1bbe245c3bb16a2534404c82
Nick Sutterer <apotonick@gmail.com>
2014-10-31 12:18:42 +1100

    don't run CustomValidation test for 3.0.

--- test/custom_validation_test.rb
+++ test/custom_validation_test.rb
@@ -1,44 +1,47 @@
 require 'test_helper'
 
-class UnexistantTitleValidator < ActiveModel::Validator
-  def validate record
-    if record.title == 'unexistant_song'
-      record.errors.add(:title, 'this title does not exist!')
+unless ActiveModel::VERSION::MAJOR == 3 and ActiveModel::VERSION::MINOR == 0
+
+  class UnexistantTitleValidator < ActiveModel::Validator
+    def validate record
+      if record.title == 'unexistant_song'
+        record.errors.add(:title, 'this title does not exist!')
+      end
     end
   end
-end
 
-class CustomValidationTest < MiniTest::Spec
+  class CustomValidationTest < MiniTest::Spec
 
-  class Album
-    include ActiveModel::Validations
-    attr_accessor :title, :artist
+    class Album
+      include ActiveModel::Validations
+      attr_accessor :title, :artist
 
-    validates_with UnexistantTitleValidator
-  end
-  
-  class AlbumForm < Reform::Form
-    extend ActiveModel::ModelValidations
-
-    property :title
-    property :artist_name, as: :artist
-    copy_validations_from Album
-  end
-  
-  let(:album) { Album.new }
-
-  describe 'non-composite form' do
-
-    let(:album_form) { AlbumForm.new(album) }
-
-    it 'is not valid when title is unexistant_song' do
-      album_form.validate(artist_name: 'test', title: 'unexistant_song').must_equal false
-    end
-    
-    it 'is valid when title is something existant' do
-      album_form.validate(artist_name: 'test', title: 'test').must_equal true
+      validates_with UnexistantTitleValidator
     end
 
-  end
+    class AlbumForm < Reform::Form
+      extend ActiveModel::ModelValidations
 
+      property :title
+      property :artist_name, as: :artist
+      copy_validations_from Album
+    end
+
+    let(:album) { Album.new }
+
+    describe 'non-composite form' do
+
+      let(:album_form) { AlbumForm.new(album) }
+
+      it 'is not valid when title is unexistant_song' do
+        album_form.validate(artist_name: 'test', title: 'unexistant_song').must_equal false
+      end
+
+      it 'is valid when title is something existant' do
+        album_form.validate(artist_name: 'test', title: 'test').must_equal true
+      end
+
+    end
+
+  end
 end
