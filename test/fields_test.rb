3379afed7a9f4a05bc8702fdfbced97337c399b4
Nick Sutterer <apotonick@gmail.com>
2015-05-05 15:42:04 +1000

    fix fields test.

--- test/fields_test.rb
+++ test/fields_test.rb
@@ -6,19 +6,9 @@ class FieldsTest < MiniTest::Spec
       fields = Reform::Contract::Fields.new([:name, :title])
       fields.name.must_equal  nil
       fields.title.must_equal nil
-    end
 
-    it "accepts list of properties and values" do
-      fields = Reform::Contract::Fields.new(["name", "title"], "title" => "The Body")
-      fields.name.must_equal  nil
-      fields.title.must_equal "The Body"
-    end
-
-    it "processes value syms" do
-      skip "we don't need to test this as representer.to_hash always returns strings"
-      fields = Reform::Fields.new(["name", "title"], :title => "The Body")
-      fields.name.must_equal  nil
-      fields.title.must_equal "The Body"
+      fields.title= "Planet X"
+      fields.title.must_equal "Planet X"
     end
   end
 end
