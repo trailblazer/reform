083679eefa8d804d9c4689df6fdf388e75aee77b
Nick Sutterer <apotonick@gmail.com>
2015-05-20 09:00:41 +1000

    :prepopulate -> (model, options) is the new API for :prepopulator lambdas. we have access to everything since the block/method is invoked in form instance context, so we don't need to pass the Options object.

--- lib/reform/form/prepopulate.rb
+++ lib/reform/form/prepopulate.rb
@@ -1,7 +1,8 @@
-# this will soon be handled in Disposable.
+# prepopulate!(options)
+# prepopulator: ->(model, user_options)
 module Reform::Form::Prepopulate
-  def prepopulate!
-    prepopulate_representer.new(self).to_object
+  def prepopulate!(options={})
+    prepopulate_representer.new(self).to_object(options)
 
     # recursive_prepopulate_representer.new(self).to_hash # not sure if i leave that like this, consider private.
     self
@@ -30,6 +31,11 @@ private
 
   class Prepopulator < Reform::Form::Populator
   private
+    def call!(form, fragment, model, options)
+      # FIXME: use U:::Value.
+      form.instance_exec(model, options.user_options, &@user_proc) # pass user_options, we got access to everything.
+    end
+
     def handle_fail(twin, options)
       # TODO: implement,
       # e.g. collections may return [] instead of one twin.
--- test/prepopulator_test.rb
+++ test/prepopulator_test.rb
@@ -5,49 +5,13 @@ class PrepopulatorTest < MiniTest::Spec
   Band = Struct.new(:name)
 
   class AlbumForm < Reform::Form
-    property :title, prepopulator: ->(*){ self.title = "Another Day At Work" }
+    property :title, prepopulator: ->(*){ self.title = "Another Day At Work" }                  # normal assignment.
     property :length
 
-    property :hit, prepopulator: ->(*){ self.hit = Song.new } do
+    property :hit, prepopulator: ->(model, options) { self.hit = Song.new(options[:title]) } do # use user options.
       property :title
 
-      property :band, prepopulator: ->(*){ self.band = Band.new } do
-        property :name
-      end
-    end
-
-    collection :songs, prepopulator: ->(*){ self.songs = [Song.new, Song.new] } do
-      property :title
-    end
-  end
-
-  it "ficken" do
-    form = AlbumForm.new(OpenStruct.new(length: 1)).prepopulate!
-
-    form.length.must_equal 1
-    form.title.must_equal "Another Day At Work"
-    form.hit.model.must_equal Song.new
-    form.songs.size.must_equal 2
-    form.songs[0].model.must_equal Song.new
-    form.songs[1].model.must_equal Song.new
-    form.songs[1].model.must_equal Song.new
-    # prepopulate works more than 1 level, recursive.
-    form.hit.band.model.must_equal Band.new
-  end
-end
-
-
-class PrepopulateInFormContextTest < MiniTest::Spec
-  Song = Struct.new(:title, :band, :length)
-  Band = Struct.new(:name)
-
-  class AlbumForm < Reform::Form
-    property :title, prepopulator: ->(options){ "#{my_title} #{options.class}" }
-
-    property :hit, prepopulator: ->(options){ my_hit } do
-      property :title
-
-      property :band, prepopulator: ->(options){ my_band } do
+      property :band, prepopulator: ->(*){ self.band = my_band } do                             # invoke your own code.
         property :name
       end
 
@@ -56,36 +20,38 @@ class PrepopulateInFormContextTest < MiniTest::Spec
       end
     end
 
-    def my_title
-      "Rhode Island Shred"
-    end
-
-    def my_hit
-      Song.new
-    end
-  end
-
-  subject { AlbumForm.new(OpenStruct.new).prepopulate! }
-
-  it { subject.title.must_equal "Rhode Island Shred Hash" }
-  it { subject.hit.model.must_equal Song.new }
-  it { subject.hit.band.model.must_equal Band.new }
-end
-
-class PrepopulateWithExistingCollectionTest < MiniTest::Spec
-  Song = Struct.new(:title)
-
-  class AlbumForm < Reform::Form
-    collection :songs, prepopulator: ->(*){ songs.map(&:model) + [Song.new] } do
-      property :title
+    collection :songs, prepopulator: ->(model, options) {
+      if model == nil
+        self.songs = [Song.new, Song.new]
+      else
+        model.insert(songs.size, Song.new)
+      end  } do
+        property :title
     end
   end
 
-  subject { AlbumForm.new(OpenStruct.new(songs: [Song.new])).prepopulate! }
+  it do
+    form = AlbumForm.new(OpenStruct.new(length: 1)).prepopulate!(title: "Potemkin City Limits")
 
-  it { subject.songs.size.must_equal 2 }
-  it { subject.songs[0].model.must_equal Song.new }
-  it { subject.songs[1].model.must_equal Song.new }
+    form.length.must_equal 1
+    form.title.must_equal "Another Day At Work"
+    form.hit.model.must_equal Song.new("Potemkin City Limits")
+    form.songs.size.must_equal 2
+    form.songs[0].model.must_equal Song.new
+    form.songs[1].model.must_equal Song.new
+    form.songs[1].model.must_equal Song.new
+    # prepopulate works more than 1 level, recursive.
+    form.hit.band.model.must_equal Band.new
+  end
+
+  # add to existing collection.
+  it do
+    form = AlbumForm.new(OpenStruct.new(songs: [Song.new])).prepopulate!
+
+    form.songs.size.must_equal 2
+    form.songs[0].model.must_equal Song.new
+    form.songs[1].model.must_equal Song.new
+  end
 end
 
 # calling form.prepopulate! shouldn't crash.
