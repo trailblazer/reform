5ebed175fc37c727764f536c672cb5fb36f50cb5
Celso Fernandes <fernandes@zertico.com>
2015-06-15 00:47:05 -0300

    Improve unique validater on model update

--- lib/reform/form/validation/unique_validator.rb
+++ lib/reform/form/validation/unique_validator.rb
@@ -1,7 +1,14 @@
 class Reform::Form::UniqueValidator < ActiveModel::EachValidator
   def validate_each(form, attribute, value)
-    if form.model.class.where("#{attribute} = ?", value).size > 0
-      form.errors.add attribute, "#{attribute} must be unique."
+    if form.model.persisted?
+      if form.model.class.where("#{attribute} = ? AND id <> ?", value, form.model.id).size > 0
+        form.errors.add attribute, "#{attribute} must be unique."
+      end
+    else
+      if form.model.class.where("#{attribute} = ?", value).size > 0
+        form.errors.add attribute, "#{attribute} must be unique."
+      end
     end
   end
 end
+
