20904af8fe631a1ae8492c5335f002610b7b8cd4
Nick Sutterer <apotonick@gmail.com>
2015-07-07 15:05:07 +1000

    use delegates.`

--- lib/reform/form/active_model/validations.rb
+++ lib/reform/form/active_model/validations.rb
@@ -3,38 +3,28 @@ require "reform/contract/errors"
 
 module Reform::Form::ActiveModel
   # AM::Validations for your form.
+  #
+  # Note: The preferred way for validations should be Lotus::Validations, as ActiveModel::Validation's implementation is
+  # old, very complex given that it needs to do a simple thing, and it's using globals like @errors.
   module Validations
     def self.included(includer)
-      # includer.send(:include, ::ActiveModel::Validations)
       includer.send(:include, Reform::Contract::Validate)
-
-      includer.extend ClassMethods
 
       includer.instance_eval do
         extend Uber::InheritableAttr
         inheritable_attr :validator
         self.validator = Class.new(Validator)
+
+        class << self
+          extend Uber::Delegates
+          delegates :validator, :validates, :validate, :validates_with, :validate_with
+        end
       end
     end
 
     # Builder.
     def errors_for_validate
       Reform::Contract::Errors.new(self)
-    end
-
-    module ClassMethods
-      def validates(*args)
-        validator.validates(*args)
-      end
-      def validate(*args)
-        validator.validate(*args)
-      end
-      def validate_with(*args)
-        validator.validate_with(*args)
-      end
-      def validates_with(*args)
-        validator.validates_with(*args)
-      end
     end
 
 
