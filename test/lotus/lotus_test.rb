31cf610f89987f8ea51f4245dc059471b9257a82
Nick Sutterer <apotonick@gmail.com>
2015-07-08 15:56:38 +1000

    add a separate lotus test directory.

---
+++ test/lotus/lotus_test.rb
@@ -0,0 +1,27 @@
+require "reform"
+require "reform/form/lotus"
+require "minitest/autorun"
+
+Reform::Contract.class_eval do
+  include Reform::Contract::Validate
+  include Reform::Form::Lotus
+end
+
+class LotusTest < Minitest::Spec
+  Album = Struct.new(:title, :songs, :artist)
+
+  class AlbumForm < Reform::Form
+
+
+    property :title
+    validates :title, presence: true
+  end
+
+  it do
+    form = AlbumForm.new(Album.new("Show Completo"))
+
+    form.validate(title: "").must_equal false
+
+    form.errors.to_s.must_equal ""
+  end
+end
