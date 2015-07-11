cc6d1681eb1cc082bf2cdc50a9a495402f995efc
Nick Sutterer <apotonick@gmail.com>
2015-07-12 09:07:01 +1000

    some more delegations to make AM::Validations with translations happy.

--- lib/reform/form/active_model/validations.rb
+++ lib/reform/form/active_model/validations.rb
@@ -5,13 +5,16 @@ require "uber/delegates"
 module Reform::Form::ActiveModel
   # AM::Validations for your form.
   #
-  # Note: The preferred way for validations should be Lotus::Validations, as ActiveModel::Validation's implementation is
-  # old, very complex given that it needs to do a simple thing, and it's using globals like @errors.
+  # Note: The preferred way for validations should be Lotus::Validations, as ActiveModel::Validation's
+  # implementation is old, very complex given that it needs to do a simple thing, is using
+  # globals like @errors, and relies and more than 100 methods to be mixed into your form.
   #
   # Implements ::validates and friends, and #valid?.
+  #
   module Validations
     def self.included(includer)
       includer.instance_eval do
+        include Reform::Form::ActiveModel
         extend Uber::InheritableAttr
         inheritable_attr :validator
         self.validator = Class.new(Validator)
@@ -19,12 +22,23 @@ module Reform::Form::ActiveModel
         class << self
           extend Uber::Delegates
           delegates :validator, :validates, :validate, :validates_with, :validate_with
+
+          # Hooray! Delegate back to Reform's Validator class which contains AM::Validations.
+          def human_attribute_name(name)
+            validator.human_attribute_name(name)
+          end
         end
       end
     end
 
     def build_errors
       Reform::Contract::Errors.new(self)
+    end
+
+    # The concept of "composition" has still not arrived in Rails core and they rely on 400 methods being
+    # available in one object. This is why we need to provide parts of the I18N API in the form.
+    def read_attribute_for_validation(name)
+      send(name)
     end
 
 
@@ -44,9 +58,6 @@ module Reform::Form::ActiveModel
         @form.send(method_name, *args, &block)
       end
 
-      # def self.model_name # FIXME: this is only needed for i18n, it seems.
-      #   "Reform::Form"
-      # end
       def self.model_name
         ActiveModel::Name.new(Reform::Form)
       end
@@ -57,7 +68,8 @@ module Reform::Form::ActiveModel
       end
     end
 
-
+    # Needs to be implemented by every validation backend and implements the
+    # actual validation. See Reform::Form::Lotus, too!
     def valid?
       validator = self.class.validator.new(self)
       validator.valid? # run the Validations object's validator with the form as context. this won't pollute anything in the form.
