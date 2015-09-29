6504baeedf43e154b5706dc1e29bc367145ec06d
Nick Sutterer <apotonick@gmail.com>
2015-09-29 18:17:46 +1000

    make #deserialize public. remove #update! cause we can.

--- lib/reform/form/validate.rb
+++ lib/reform/form/validate.rb
@@ -19,8 +19,6 @@ module Reform::Form::Validate
   # 2. Deserialize. This is wrong and should be done in 1.
   # 3. Validate the form object graph.
   def validate(params)
-    deprecate_update!(params)
-
     # allow an external deserializer.
     block_given? ? yield(params) : deserialize(params)
 
@@ -29,6 +27,12 @@ module Reform::Form::Validate
   #   raise DeserializeError.new("[Reform] Deserialize error: You probably called #validate without setting up your nested models. Check https://github.com/apotonick/reform#populating-forms-for-validation on how to use populators.")
   end
 
+  def deserialize(params)
+    params = deserialize!(params)
+    deserializer.new(self).from_hash(params)
+  end
+
+private
   # Meant to return params processable by the representer. This is the hook for munching date fields, etc.
   def deserialize!(params)
     # NOTE: it is completely up to the form user how they want to deserialize (e.g. using an external JSON-API representer).
@@ -36,24 +40,6 @@ module Reform::Form::Validate
       # e.g. adding new items in collections using #<< etc.
     # DISCUSS: using self here will call the form's setters like title= which might be overridden.
     params
-  end
-
-
-private
-  # Some users use this method to pre-populate a form. Not saying this is right, but we'll keep
-  # this method here.
-  # DISCUSS: this is only called once, on the top-level form.
-  def deprecate_update!(params)
-    return unless self.class.instance_methods(false).include?(:update!)
-    warn "[Reform] Form#update! is deprecated and will be removed in Reform 2.1. Please use #prepopulate!"
-    update!(params)
-  end
-
-  def deserialize(params)
-    puts "deserialize called for #{self}!"
-    params = deserialize!(params)
-
-    deserializer.new(self).from_hash(params)
   end
 
   # Default deserializer for hash.
@@ -64,15 +50,9 @@ private
       superclass:       Representable::Decorator,
       representer_from: lambda { |inline| inline.representer_class },
       options_from:     :deserializer,
-      exclude_options:  [:default], # Reform must not copy Disposable/Reform-only options that might confuse representable.
+      exclude_options:  [:default, :populator], # Reform must not copy Disposable/Reform-only options that might confuse representable.
     ) do |dfn|
-      dfn.delete!(:populator) # FIXME: exclude
-
-      dfn.merge!(
-        deserialize: lambda { |form, params, options|
-          form.send(:deserialize, params)
-        }
-      ) if dfn[:twin]
+      dfn.merge!(deserialize: ->(form, params, *) { form.send(:deserialize, params) }) if dfn[:twin]
     end
 
     deserializer
