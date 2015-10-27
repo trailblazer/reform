3d661aa8efbff220504cfff4d0e119b17731fe56
Annie Caron <annie.caron@cgi.com>
2015-10-27 08:40:07 -0400

    Add documentation to unique validation

--- lib/reform/form/validation/unique_validator.rb
+++ lib/reform/form/validation/unique_validator.rb
@@ -1,4 +1,33 @@
-# Reform's own implementation for uniqueness which does not write to model.
+# === Unique Validation
+# Reform's own implementation for uniqueness which does not write to model
+#
+# == Usage
+# Pass a true boolean value to validate a field against all values available in
+# the database:
+# validates :title, unique: true
+#
+# == Options
+# = Scope
+# A scope can be use to filter the records that need to be compare with the
+# current value to validate. A scope array can have one to many fields define.
+#
+# A scope can be define the following ways:
+# validates :title, unique: { scope: :album_id }
+# validates :title, unique: { scope: [:album_id] }
+# validates :title, unique: { scope: [:album_id, ...] }
+#
+# All fields included in a scope must be declared as a property like this:
+# property :album_id
+# validates :title, unique: { scope: :album_id }
+#
+# Just remove write access to the property if the field must not be change:
+# property :album_id, writeable: false
+# validates :title, unique: { scope: :album_id }
+#
+# This use case is useful if album_id is set to a Song this way:
+# song = album.songs.new
+# album_id is automatically set and can't be change by the operation
+
 class Reform::Form::UniqueValidator < ActiveModel::EachValidator
   def validate_each(form, attribute, value)
     model = form.model_for_property(attribute)
