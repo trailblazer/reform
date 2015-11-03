808400fd0c50c7ef3dc258abdf61bb7891c717ea
Nick Sutterer <apotonick@gmail.com>
2015-11-03 21:35:59 +1100

    use new declarative.

--- lib/reform/form/module.rb
+++ lib/reform/form/module.rb
@@ -3,13 +3,13 @@ module Reform::Form::Module
   def self.included(base)
     base.extend ClassMethods
 
-    base.extend Declarative::DSL # FIXME: should be H:DSL
-    base.extend Declarative::Inheritance # FIXME: should be H:ModuleInclusion
+    base.extend Declarative::Heritage::DSL      # ::heritage
+    base.extend Declarative::Heritage::Included # ::included
   end
 
   module ClassMethods
     def method_missing(method, *args, &block)
-      heritage << { method: method, args: args, block: block }
+      heritage.record(method, *args, &block)
     end
   end
 end
