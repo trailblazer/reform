04c4063a0a87073811ca2170147540570b31dfcf
Nick Sutterer <apotonick@gmail.com>
2016-07-29 13:27:56 +0200

    Deprecated positional arguments for `validation :default, options: {}`. New API: `validation name: :default, **`.

--- lib/reform/validation.rb
+++ lib/reform/validation.rb
@@ -7,21 +7,24 @@ module Reform::Validation
     end
 
     # DSL.
-    def validation(*args, &block)
-      if args[0].is_a? Hash
-        options = args[0]
-      else
-        name, options = args
-      end
-
-      name ||= :default
-      options ||= {}
-
+    def validation(name=:default, options={}, &block)
       heritage.record(:validation, name, options, &block)
+
+      options = deprecate_validation_positional_args(name, options)
+      name    = options[:name] # TODO: remove in favor of kw args in 3.0.
 
       group = validation_groups.add(name, options)
 
       group.instance_exec(&block)
+    end
+
+    def deprecate_validation_positional_args(name, options)
+      if name.is_a?(Symbol)
+        warn "[Reform] Form::validation API is now: validation(name: :default, if:nil, schema:Schema). Please use keyword arguments instead of positional arguments."
+        return { name: name }.merge(options)
+      end
+
+      { name: :default }.merge(name)
     end
   end
 
