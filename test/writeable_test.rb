31746ad9c8de86fe026f13b808f2c0a68b2c54bd
sot528 <sot528@gmail.com>
2016-09-08 01:29:59 +0900

    Add alias to writeable property option. (#393)
    * Add writable as alias to `writeable` property option.
    
    * Add LF.
    
    * give priority to `:writeable` option.
    
    * fix typo.

--- test/writeable_test.rb
+++ test/writeable_test.rb
@@ -27,3 +27,32 @@ class WriteableTest < MiniTest::Spec
     hash.must_equal("country"=> "Germany")
   end
 end
+
+# writable option is alias of writeable option.
+class WritableTest < MiniTest::Spec
+  Location = Struct.new(:country)
+
+  class LocationForm < Reform::Form
+    property :country, writable: false
+  end
+
+  let (:loc) { Location.new("Australia") }
+  let (:form) { LocationForm.new(loc) }
+
+  it do
+    form.country.must_equal "Australia"
+
+    form.validate("country" => "Germany") # this usually won't change when submitting.
+    form.country.must_equal "Germany"
+
+    form.sync
+    loc.country.must_equal "Australia" # the writer wasn't called.
+
+    hash = {}
+    form.save do |nested|
+      hash = nested
+    end
+
+    hash.must_equal("country"=> "Germany")
+  end
+end
