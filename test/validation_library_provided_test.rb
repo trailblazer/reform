50ff46bb17a6b0d28631b961ece79bd912193e56
Fran Worley <frances@safetytoolbox.co.uk>
2016-11-11 13:50:37 +0000

    add a custom error when no validation library is provided

---
+++ test/validation_library_provided_test.rb
@@ -0,0 +1,16 @@
+require 'reform'
+require 'minitest/autorun'
+
+class ValidationLibraryProvidedTest < MiniTest::Spec
+  it 'no validation library loaded' do
+    assert_raises Reform::Validation::NoValidationLibraryError do
+      class PersonForm < Reform::Form
+        property :name
+
+        validation do
+          required(:name).maybe(:str?)
+        end
+      end
+    end
+  end
+end
