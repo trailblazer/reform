f3429248bead08939cc32de8648a94e7bafd9ee5
Nick Sutterer <apotonick@gmail.com>
2017-01-28 23:18:46 +1100

    simplify #validate! by simply returning the errors instead of writing to this weird errors object on the go.

--- lib/reform/contract/validate.rb
+++ lib/reform/contract/validate.rb
@@ -4,7 +4,7 @@ module Reform::Contract::Validate
   #   @errors = build_errors
   # end
 
-  attr_reader :errors
+  attr_reader :errors # TODO: breaks when #validate wasn't called (and that's a GOOD THING.)
 
   def validate
     errors = validate!([])
@@ -13,49 +13,30 @@ module Reform::Contract::Validate
   end
 
   def validate!(prefix)
-    validate_nested!(prefix)
+    nested = validate_nested!(prefix)
 
     local_errors = build_errors
     Reform::Validation::Groups::Result.(self.class.validation_groups, self, local_errors)
 
-
-
-
-
-
+    nested.each do |(name, errors)|
+      local_errors.merge!(errors, [name])
+    end
 
     @errors = local_errors # @ivar sucks, of course
-
-    # Group.() #=> errs={title: "bla wrong"}
-    # @errors = errs
-    # @errors, nested_errors = validate_branch(prefix)
-
-    # puts "----> #{@errors}"
-    # puts "----> #{nested_errors}"
-
-
-    # errors.merge!(@errors, prefix) # local errors.
-    # errors.merge!(nested_errors, [])
-
-    @errors
   end
 
   # TODO: make build_errors a lambda.
-  # def validate_branch(prefix)
-  #   validate_nested!(nested_errors = build_errors, prefix)
-
-
-
-  #   [local_errors, nested_errors]
-  # end
 
 private
 
   # runs form.validate! on all nested forms
   def validate_nested!(prefixes)
+    arr = []
     schema.each(twin: true) do |dfn|
       # recursively call valid? on nested form.
-      Disposable::Twin::PropertyProcessor.new(dfn, self).() { |form| form.validate!(prefixes+[dfn[:name]]) }
+      Disposable::Twin::PropertyProcessor.new(dfn, self).() { |form| arr << [dfn[:name], form.validate!(prefixes+[dfn[:name]])] }
     end
+
+    arr
   end
 end
