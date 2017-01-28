332e8010d6946dd7d94c1ed374d494f01ea4183f
Nick Sutterer <apotonick@gmail.com>
2017-01-28 23:21:12 +1100

    remove prefixes.

--- lib/reform/contract/validate.rb
+++ lib/reform/contract/validate.rb
@@ -7,13 +7,13 @@ module Reform::Contract::Validate
   attr_reader :errors # TODO: breaks when #validate wasn't called (and that's a GOOD THING.)
 
   def validate
-    errors = validate!([])
+    errors = validate!
 
     errors.empty?
   end
 
-  def validate!(prefix)
-    nested = validate_nested!(prefix)
+  def validate!
+    nested = validate_nested!
 
     local_errors = build_errors
     Reform::Validation::Groups::Result.(self.class.validation_groups, self, local_errors)
@@ -30,11 +30,11 @@ module Reform::Contract::Validate
 private
 
   # runs form.validate! on all nested forms
-  def validate_nested!(prefixes)
+  def validate_nested!
     arr = []
     schema.each(twin: true) do |dfn|
       # recursively call valid? on nested form.
-      Disposable::Twin::PropertyProcessor.new(dfn, self).() { |form| arr << [dfn[:name], form.validate!(prefixes+[dfn[:name]])] }
+      Disposable::Twin::PropertyProcessor.new(dfn, self).() { |form| arr << [dfn[:name], form.validate!] }
     end
 
     arr
