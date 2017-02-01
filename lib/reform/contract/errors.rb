3592633a9f79a468d9d31e1ea5305b4674688bdd
Nick Sutterer <apotonick@gmail.com>
2017-02-01 10:11:52 +0100

    remove Errors until we need it back.

--- lib/reform/contract/errors.rb
+++ lib/reform/contract/errors.rb
@@ -64,42 +64,7 @@ module Reform
   end
 end
 
-class Reform::Contract::Errors
-  def initialize(original_result=nil)
-    @original_result = original_result
-    @errors = {}
-    @full_errors = Set.new
-  end
-
-  def add(field, message)
-    @errors[field] ||= []
-    @errors[field] << message
-
     # Ensure that we can return Active Record compliant full messages when using dry
     # we only want unique messages in our array
-    human_field = field.to_s.gsub(/([\.\_])+/, " ").gsub(/(\b\w)+/) { |s| s.capitalize }
-    @full_errors.add("#{human_field} #{message}")
-  end
-
-  def messages
-    @errors
-  end
-
-  def full_messages
-    @full_errors.to_a
-  end
-
-  # needed by Rails form builder.
-  def [](name)
-    @errors[name] || []
-  end
-
-  def success?
-    size.zero?
-  end
-
-private
-  def size
-    @errors.values.flatten.size
-  end
-end
+    # human_field = field.to_s.gsub(/([\.\_])+/, " ").gsub(/(\b\w)+/) { |s| s.capitalize }
+    # @full_errors.add("#{human_field} #{message}")
