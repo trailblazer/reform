afb29089c295adb395c72e357ce8bfa5a245f8f0
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2019-08-19 15:38:38 +1000

    Fix NoMethod error for virtual collections (#485)
    * Add as default empty array if it's a vitual collection
    
    * We are not running rubocop in travis so https rubocop file is not required anymore
    
    * Ignore byebug_history
    
    * Update rubocop stuff

--- test/virtual_test.rb
+++ test/virtual_test.rb
@@ -3,14 +3,18 @@ require "test_helper"
 class VirtualTest < MiniTest::Spec
   class CreditCardForm < TestForm
     property :credit_card_number, virtual: true # no read, no write, it's virtual.
+    collection :transactions, virtual: true, populate_if_empty: OpenStruct do
+      property :id
+    end
   end
 
   let(:form) { CreditCardForm.new(Object.new) }
 
   it {
-    form.validate("credit_card_number" => "123")
+    form.validate(credit_card_number: "123", transactions: [id: 1])
 
-    form.credit_card_number.must_equal "123"  # this is still readable in the UI.
+    form.credit_card_number.must_equal "123" # this is still readable in the UI.
+    form.transactions.first.id.must_equal 1 # this is still readable in the UI.
 
     form.sync
 
@@ -19,6 +23,42 @@ class VirtualTest < MiniTest::Spec
       hash = nested
     end
 
-    hash.must_equal("credit_card_number" => "123")
+    hash.must_equal("credit_card_number" => "123", "transactions" => ["id" => 1])
+  }
+end
+
+class VirtualAndDefaultTest < MiniTest::Spec
+  class CreditCardForm < TestForm
+    property :credit_card_number, virtual: true, default: "123" # no read, no write, it's virtual.
+    collection :transactions, virtual: true, populate_if_empty: OpenStruct, default: [OpenStruct.new(id: 2)] do
+      property :id
+    end
+  end
+
+  def hash(form)
+    hash = {}
+    form.save do |nested|
+      hash = nested
+    end
+    hash
+  end
+
+  let(:form) { CreditCardForm.new(Object.new) }
+
+  it {
+    form = CreditCardForm.new(Object.new)
+    form.validate({})
+
+    hash(form).must_equal("credit_card_number" => "123", "transactions" => ["id" => 2])
+
+    form = CreditCardForm.new(Object.new)
+    form.validate(credit_card_number: "123", transactions: [id: 1])
+
+    form.credit_card_number.must_equal "123" # this is still readable in the UI.
+    form.transactions.first.id.must_equal 1 # this is still readable in the UI.
+
+    form.sync
+
+    hash(form).must_equal("credit_card_number" => "123", "transactions" => ["id" => 1])
   }
 end
