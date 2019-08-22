63961a269aa9a7cecf6b76d1f7a3179691a808ca
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2019-08-23 08:23:36 +1000

    Removed code that is in reform-rails now

--- lib/reform/mongoid.rb
+++
@@ -1,4 +0,0 @@
-require 'reform/form/active_model'
-require 'reform/form/orm'
-require 'reform/form/mongoid'
-require 'reform/form/active_model/model_reflections' # only load this in AR context as simple_form currently is bound to AR.
